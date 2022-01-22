2d02a4a2991bbb7cd16973fc797898f940cb958a
taekgyu <taekgyu0602@gmail.com>
2022-01-22 17:10:00 +0900

    Initial Commit

---
+++ src/main/java/com/taek/w3spring/W3springApplication.java
@@ -0,0 +1,13 @@
+package com.taek.w3spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class W3springApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(W3springApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taek/w3spring/W3springApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taek.w3spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3springApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
